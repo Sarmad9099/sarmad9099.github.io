a893356e097848596e0f0f134640071a92a5f97f
Sarmad9099 <140377374+Sarmad9099@users.noreply.github.com>
2023-07-24 15:05:11 +0500

    Update script.js

--- week4/script.js
+++ week4/script.js
@@ -1,9 +1,9 @@
 var names=new Array();
 names[0]="Yaakov";
-names[1]="James";
-names[2]="Jin";
-names[3]="jake";
-names[4]="polard";
+names[1]="John";
+names[2]="Jen";
+names[3]="jason";
+names[4]="paul";
 names[5]="frank";
 names[6]="larry";
 names[7]="paula";
